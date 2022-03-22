dae94444a8b4e5477d9fd30a30e539525f7af5f2
Kiran-Malve <kiranmalve99@gmail.com>
2022-03-22 09:02:34 +0530

    Todo application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup";
 
-function App() {
+export function App() {
+  const [taskValue, setTaskValue] = useState("");
+  const [lists, setList] = useState([]);
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setTaskValue(e.target.value);
+  };
+  const addItem = () => {
+    if (taskValue !== "") {
+      let userInput = {
+        id: Math.random(),
+        value: taskValue,
+      };
+
+      setList((prev) => {
+        return [...prev, userInput];
+      });
+      setTaskValue("");
+    }
+  };
+
+  console.log("todo list", lists);
+  const deleteItem = (itemId) => {
+    console.log("item", itemId);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "3rem",
+          fontWeight: "bolder",
+        }}
+      >
+        TODO LIST
+      </Row>
+      <hr />
+      <Row>
+        <Col md={{ span: 5, offset: 4 }}>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="add item . . . "
+              size="lg"
+              value={taskValue}
+              onChange={handleChange}
+              aria-label="add something"
+              aria-describedby="basic-addon2"
+            />
+
+            <Button variant="dark" size="lg" onClick={() => addItem()}>
+              ADD
+            </Button>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 5, offset: 4 }}>
+          <ListGroup>
+            {/* map over and print items */}
+            {lists.map((item, i) => {
+              return (
+                <ListGroup.Item
+                  variant="dark"
+                  action
+                  key={i}
+                  onClick={() => deleteItem(item.id)}
+                >
+                  {item.value}
+                </ListGroup.Item>
+              );
+            })}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
-export default App;
